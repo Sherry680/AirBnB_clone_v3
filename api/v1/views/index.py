fcd07bdfb9d44fed2853cc79e23dce46c270635d
muasyasila <curtissila4@gmail.com>
2023-05-01 12:24:17 +0300

    Task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""index --connect to API"""
+from api.v1.views import app_views
+from models import storage
+from flask import Flask, Blueprint, jsonify
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+@app_views.route('/status')
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+if __name__ == "__main__":
+    pass
